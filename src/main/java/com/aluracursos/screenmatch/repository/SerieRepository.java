9f13e3df0179d467a14f735f88f25b9fbde876b6
Angel66694 <162082184+Angel66694@users.noreply.github.com>
2024-05-20 01:01:43 -0600

    actualizacion de seriesRepository

--- src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -17,8 +17,7 @@ public interface SerieRepository extends JpaRepository<Serie,Long> {
     @Query("SELECT s FROM Serie s WHERE s.totalDeTemporadas >= :totalDeTemporadas AND s.evaluacion >= :evaluacion")
     List<Serie> seriesPorTemporadaYEvaluacion(int totalDeTemporadas, Double evaluacion);
 
-   // @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:nombreEpisodio%")
-    //List<Episodio> episodiosPorNombre(String nombreEpisodio);
+
     @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE LOWER(e.titulo) LIKE LOWER(CONCAT('%', :nombreEpisodio, '%'))")
     List<Episodio> episodiosPorNombre(String nombreEpisodio);
 
